b929bbd73699d3c94a72a2759aefaa90c4769160
hullaah <umaradelo1.247@gmail.com>
2023-09-22 16:37:58 +0100

    Added description for all puppets file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,4 @@
+# creates a file, school, in /tmp with permisiion 0744, owner www-data, group www-data and content 'I love Puppet'.
 file { '/tmp/school':
     ensure  => present,
     path    => '/tmp/school',
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,4 @@
+# kills the procss killmenow
 exec { 'killmenow':
   command => 'pkill killmenow',
   path    => ['/bin', '/usr/bin', '/usr/local/bin'],
