1019e526bede436e5bfac88e248b929e23bb0e44
hullaah <umaradelo1.247@gmail.com>
2023-09-25 23:17:57 +0100

    fixed the puppet file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # modifies the ssh configurayion file
-file {'/home/hullaah/.ssh/config':
+file {'/etc/ssh/ssh_config':
   ensure  => present,
-  path    => '/home/hullaah/.ssh/config',
+  path    => '/etc/ssh/ssh_config',
   content => "Host 54.160.84.211\n\tIdentityFile /home/ubuntu/.ssh/school\n\tPasswordAuthentication no"
 }
