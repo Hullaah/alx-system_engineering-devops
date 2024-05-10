504fafaa0b1872cc7515e431ca83d49047c4d1e2
Hullaah <umaradelo1.247@gmail.com>
2024-05-10 10:49:31 +0100

    modified the tasks to work for a lower version of puppet used by the checker

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
 # installs flask from pip3
-package { 'pip3':
-  ensure => installed,
-  name   => 'python3-pip',
-}
-
 package { 'flask':
   ensure   => '2.1.0',
   name     => 'Flask',
-  require  => Package['pip3'],
   provider => 'pip3',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # kills a process named killmenow
 exec { 'killmenow':
-  command => ['kill', '-9', 'killmenow'],
+  command => 'kill -9 killmenow',
   path    => ['/bin', '/usr/bin', '/usr/local/bin'],
 }
